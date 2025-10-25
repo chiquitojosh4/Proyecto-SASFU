be30a325ebdae6c9c79c179b34ee1d0117ff14e5
Chiquitojosh4 <chiquitojosh4@gmail.com>
2025-10-24 23:31:43 -0500

    inyeccion

--- SASFU.py
+++ SASFU.py
@@ -150,6 +150,13 @@ class Inscripcion(Iniciar_fase,Finalizar_fase):#Clase Inscripcion
     def finalizar(self):#Metodo finalizar la inscripcion
         print(f"La inscripción para la carrera de {self.carrera} en la facultad de {self.facultad} ha finalizado.")
 
+#inyeccion de dependencia en proceso.josh
+class tipo_de_examen:
+    pass
+class mixto(tipo_de_examen):
+    pass
+class por_area(tipo_de_examen):
+    pass
 class Evaluacion:#Clase Evaluacion 
     def __init__(self, tipo:str, puntaje:int, horario:str, modalidad:str, sede:str):
         self.tipo = tipo
